354fe04f38e2e3fba253af634303be5d34907ea3
mcoding777 <mcoding777@naver.com>
2023-12-18 08:27:52 +0900

    feat: 슈팅게임 관련 배경 및 기본 코드 추가

---
+++ src/Shooting/Ground.tsx
@@ -0,0 +1,26 @@
+// import { useTexture } from '@react-three/drei'
+import { useTexture } from '@react-three/drei'
+import { RigidBody } from '@react-three/rapier'
+import * as THREE from 'three'
+
+export const Ground = () => {
+  const texture = useTexture(
+    'https://images.unsplash.com/photo-1525468568166-6f2cd17c7ec9?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    (current) => {
+      const curr = current as THREE.Texture
+      curr.wrapS = curr.wrapT = THREE.RepeatWrapping // 이미지를 반복해서 매핑
+      curr.repeat.x = curr.repeat.y = 360 // 텍스쳐를 몇개 반복할건지 (= 이미지 크기 조정)
+    },
+  )
+  console.log('texture => ', texture)
+
+  return (
+    <RigidBody>
+      <mesh position={[0, -5, 0]} rotation={new THREE.Euler(-Math.PI / 2)}>
+        <planeGeometry args={[500, 500]} />
+        <ambientLight intensity={1.5} />
+        <meshStandardMaterial color="gray" map={texture} />
+      </mesh>
+    </RigidBody>
+  )
+}
--- src/Shooting/ShootingGame.tsx
+++ src/Shooting/ShootingGame.tsx
@@ -1,5 +1,50 @@
+import { PointerLockControls, Sky } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Physics, RigidBody } from '@react-three/rapier'
+
+import { Ground } from './Ground'
+
 const ShootingGame = () => {
-  return <div>ShootingGame</div>
+  return (
+    <div
+      id="container"
+      style={{
+        height: '100vh',
+      }}
+    >
+      <div
+        className="aim"
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          width: 10,
+          height: 10,
+          border: '2px solid white',
+          borderRadius: '50%',
+          transform: 'translate3d(-50%, -50%, 0)',
+          zIndex: 2,
+        }}
+      />
+      <Canvas
+        camera={{
+          fov: 50, // 배율
+        }}
+      >
+        <PointerLockControls />
+        <Sky sunPosition={[100, 20, 100]} />
+        <Physics gravity={[0, -20, 0]}>
+          <Ground />
+          {/* <Player /> */}
+          <RigidBody>
+            <mesh position={[0, 3, -5]}>
+              <boxGeometry />
+            </mesh>
+          </RigidBody>
+        </Physics>
+      </Canvas>
+    </div>
+  )
 }
 
 export default ShootingGame
